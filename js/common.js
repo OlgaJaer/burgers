4438c461e78ee987ab84e6fe6c77a55c49cf8015
Olga Jaeger <olgar.mamaeva@rambler.ru>
2018-11-17 14:08:18 -0700

    add pathes

--- js/common.js
+++ js/common.js
@@ -3,15 +3,20 @@ const fixedMenu = document.querySelector(".fixed-menu");
 const closeMenu = document.querySelector(".fixed-menu__close");
 
 hamburger.addEventListener('click', function (e) {
-e.preventDefault();
-fixedMenu.classList.add('fixed-menu__open');
+  e.preventDefault();
+  fixedMenu.classList.add('fixed-menu__open');
 });
 
-closeMenu.addEventListener('mouseover', function() {
-  fixedMenu.classList.add('fixed-menu__open:hover');
-});
 
-fixedMenu.addEventListener('click', function() {
+fixedMenu.addEventListener('click', function () {
   fixedMenu.classList.remove('fixed-menu__open');
+});
+
+const triggerMenu = document.querySelector(".menu-acc__item-trigger");
+const textMenu = document.querySelector(".menu-acc__text");
+
+triggerMenu.addEventListener('click', function (e) {
+  e.preventDefault();
+  textMenu.classList.add("menu-acc__text-active");
 });
 
