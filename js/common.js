719595665b3237cd36b63fa060f2f4f9902e7811
Olga Jaeger <olgar.mamaeva@rambler.ru>
2018-11-18 15:18:17 -0700

    modified humburger-menu, footer

--- js/common.js
+++ js/common.js
@@ -5,11 +5,13 @@ const closeMenu = document.querySelector(".fixed-menu__close");
 hamburger.addEventListener('click', function (e) {
   e.preventDefault();
   fixedMenu.classList.add('fixed-menu__open');
+  document.body.classList.add('scroll-hidden');
 });
 
 
 fixedMenu.addEventListener('click', function () {
   fixedMenu.classList.remove('fixed-menu__open');
+  document.body.classList.remove('scroll-hidden');
 });
 
 const triggerMenu = document.querySelector(".menu-acc__item-trigger");
@@ -19,4 +21,3 @@ triggerMenu.addEventListener('click', function (e) {
   e.preventDefault();
   textMenu.classList.add("menu-acc__text-active");
 });
-
